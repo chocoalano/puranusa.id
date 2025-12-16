0f1b7e31f3424ed9933defb049ebd875ce44cb28
chocoalano <alangentina95@gmail.com>
2025-12-16 16:36:37 +0700

    sdfgsfdg

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Home redirect - must be before other route files
+Route::get('/', function () {
+    return redirect()->route('ecommerce.beranda');
+})->name('home');
+
 require __DIR__.'/Ecommerce/shop.php';
 require __DIR__.'/settings.php';
 require __DIR__.'/Admin/bonus.php';
 require __DIR__.'/Admin/manage.php';
 require __DIR__.'/Admin/ecommerce.php';
-
-Route::get('/', function () {
-    return redirect()->route('ecommerce.beranda');
-})->name('home');
 
