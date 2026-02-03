1d17fc8dc3c1c75dc572ce5b4d38a403803cc8ac
chocoalano <alangentina95@gmail.com>
2026-02-03 21:59:59 +0700

    update february rev.3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $nodePath = '/opt/plesk/node/23/bin';
+        putenv("PATH=" . getenv('PATH') . ":$nodePath");
         // Share settings globally with Inertia
         Inertia::share([
             'appSettings' => function () {
